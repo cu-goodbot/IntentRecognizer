a8a58613fe349a720271576a20c6dae380ef6628
ShivendraAgrawal <shivendra.agrawal@gmail.com>
2019-04-08 22:42:03 -0600

    Add basic speech to intent

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,12 +1,38 @@
+from pprint import pprint
+
 import speech_recognition as sr
-
-# Some unorganised trial code
-
 recognizer = sr.Recognizer()
+STEP_SIZE = 2
 
-with sr.Microphone() as sound_source:
-    print("Say something")
-    audio = recognizer.listen(sound_source)
-    print("Audio recorded")
+def speech_to_text():
+    with sr.Microphone() as sound_source:
+        print("Say something")
+        audio = recognizer.listen(sound_source)
+        print("Audio recorded")
+        return recognizer.recognize_google(audio).lower()
 
-print(recognizer.recognize_google(audio))
+def identify_command():
+    utterance = speech_to_text()
+
+    print("'" + utterance + "'")
+    if 'forward' in utterance:
+        return {'forward_distance' : STEP_SIZE}
+    elif 'stop' in utterance:
+        return {'forward_distance': 0}
+    elif 'left' in utterance:
+        return {'turn_distance': 1.5708}
+    elif 'right' in utterance:
+        return {'turn_distance': -1.5708}
+    elif 'goodboy' in utterance.replace(" ", ""):
+        identify_command()
+    else:
+        return {'explain': 'Sorry, can you repeat?'}
+
+
+
+def main():
+    pprint(identify_command())
+
+
+if __name__ == "__main__":
+    main()
