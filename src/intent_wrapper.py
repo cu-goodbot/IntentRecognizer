910a5d4a594efdbbdb591faf6e6d0b10b1b5e190
Shivendra Agrawal <shivendra.agrawal@gmail.com>
2019-04-21 16:10:07 -0600

    Add POI info parser

--- src/intent_wrapper.py
+++ src/intent_wrapper.py
@@ -7,7 +7,8 @@ ROS wrapper for Intent Recognition module.
 import rospy
 import json
 from std_msgs.msg import String
-from rospy_message_converter import message_converter
+# from rospy_message_converter import message_converter
+from scene_understanding.msg import Scene
 
 from speech_to_text import speech_to_text, identify_command
 
@@ -19,9 +20,12 @@ class IntentWrapper(object):
         self.utterance = None
         # flag for if input has been received
         self.input_flag = False
+        # Latest POI info
+        self.POI_info = None
 
         # create publishers and subscribers
         intent_pub = rospy.Publisher('intent',String,queue_size=10)
+        rospy.Subscriber('/scene_info', Scene, self.get_POI_info)
 
         rospy.init_node('intent_recognizer')
 
@@ -40,17 +44,35 @@ class IntentWrapper(object):
             msg.data = flat_data
             intent_pub.publish(msg)
 
+
+    def get_POI_info(self, msg):
+        """
+        Scene info callback. Gets POI info from scene_info to infer true intent
+        :return: TODO
+        """
+        self.POI_info = msg
+
     
     def get_input(self):
         """
         Wait for user input, beginning w/ button press, and turn speech into intnet.
         """
         # wait for button press
-        raw_input('Press r and then enter and then speak input')
-        utterance = identify_command()
-        # utterance = {'turn_distance': 1.5708}
+        raw_input('Press r and then enter and then speak input\n')
+        intent = identify_command()
+        intent["poi_present"] = False
+        intent["poi_depth"] = None
+        intent["poi_angle"] = None
+        # intent = {'turn_distance': 1.5708}
 
-        return utterance
+        # Add POI info to generate the true intent
+        for obstacle in self.POI_info.objects:
+            if obstacle.label == 'person':
+                intent["poi_present"] = True
+                intent["poi_depth"] = obstacle.depth
+                intent["poi_angle"] = obstacle.angle
+
+        return intent
 
 if __name__ == "__main__":
     IntentWrapper()
--- src/speech_to_text.py
+++ src/speech_to_text.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-
+# from SceneUnderstanding.src.object_detection import obstacle_detector
 import speech_recognition as sr
 recognizer = sr.Recognizer()
 STEP_SIZE = 2
@@ -9,25 +9,30 @@ def speech_to_text():
         print("Say something")
         audio = recognizer.listen(sound_source)
         print("Audio recorded")
-        return recognizer.recognize_google(audio).lower()
+        try:
+            return recognizer.recognize_google(audio).lower()
+        except:
+            return "forward"
+
 
 def identify_command():
     utterance = speech_to_text()
 
     print("'" + utterance + "'")
     if 'forward' in utterance:
-        return {'forward_distance' : STEP_SIZE}
+        return {'forward_distance' : STEP_SIZE,
+                'turn_direction': None}
     elif 'stop' in utterance:
-        return {'forward_distance': 0}
+        return {'forward_distance': 0,
+                'turn_direction': None}
     elif 'left' in utterance:
-        return {'turn_distance': 1.5708}
+        return {'turn_direction': 'left',
+                'forward_distance' : STEP_SIZE}
     elif 'right' in utterance:
-        return {'turn_distance': -1.5708}
-    elif 'goodboy' in utterance.replace(" ", ""):
-        identify_command()
+        return {'turn_direction': 'right',
+                'forward_distance' : STEP_SIZE,}
     else:
         return {'explain': 'Sorry, can you repeat?'}
-
 
 
 def main():
@@ -36,3 +41,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # print(obstacle_detector)
